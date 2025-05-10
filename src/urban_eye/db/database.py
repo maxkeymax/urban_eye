5dc0f2b29bb35969997c21fe9a8bcec828163a7d
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-05-10 19:51:52 +0300

    dev: create base.py and main.py with temp root endpoint

--- src/urban_eye/db/database.py
+++ src/urban_eye/db/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@postgres:5432/postgres"
@@ -6,11 +6,9 @@ DATABASE_URL = "postgresql+asyncpg://postgres:postgres@postgres:5432/postgres"
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-    autoflush=False
+    bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
 )
+
 
 async def get_db() -> AsyncSession:
     async with AsyncSessionLocal() as session:
