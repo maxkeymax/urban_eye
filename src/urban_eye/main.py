d5c2f71a2406e5d3ab369b2625916e5ec37bae12
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-05-12 13:51:29 +0300

    dev: add user_router to main.py

--- src/urban_eye/main.py
+++ src/urban_eye/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 
+from urban_eye.api.routers.user import router as user_router
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(user_router)
