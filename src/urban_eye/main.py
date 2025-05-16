0e88835f5c5eef406ac2f18483f821557d39107e
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-05-16 11:26:08 +0300

    add auth router to main.py

--- src/urban_eye/main.py
+++ src/urban_eye/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 from urban_eye.api.routers.user import router as user_router
 from urban_eye.api.routers.camera import router as camera_router
+from urban_eye.api.routers.auth import router as auth_router
 
 app = FastAPI()
 
 
 app.include_router(user_router)
 app.include_router(camera_router)
+app.include_router(auth_router)
