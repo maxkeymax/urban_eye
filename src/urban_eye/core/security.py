5c14dc31167d4c02d2380aad1699a74308cce7f5
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-05-16 14:14:36 +0300

    dev: add TockerGenerator to security.py and import python-jose

--- src/urban_eye/core/security.py
+++ src/urban_eye/core/security.py
@@ -1,4 +1,14 @@
+from datetime import datetime, timedelta
+
+from jose import jwt
 from passlib.context import CryptContext
+
+from urban_eye.settings import settings
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+REFRESH_TOKEN_EXPIRE_DAYS = settings.REFRESH_TOKEN_EXPIRE_DAYS
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -10,3 +20,18 @@ class Hashing:
 
     def verify_password(plain_password: str, hashed_password: str) -> bool:
         return pwd_context.verify(plain_password, hashed_password)
+
+
+class TokenGenerator:
+    def create_access_token(data: dict):
+        to_encode = data.copy()
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode.update({"exp": expire})
+        return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    def create_refresh_token(data: dict):
+        to_encode = data.copy()
+        expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+        to_encode.update({"exp": expire})
+        return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
