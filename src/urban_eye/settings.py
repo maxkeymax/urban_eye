2bc5b246acce48b0400e4f6cf695d9972d0c3eb0
Max Klyuchnikov <me.klyuchnikov@gmail.com>
2025-05-17 12:33:37 +0300

    dev: add minio settings

--- src/urban_eye/settings.py
+++ src/urban_eye/settings.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    
+    MINIO_AWS_ACCESS_KEY_ID: str = 'minioadmin'
+    MINIO_SECRET_ACCESS_KEY: str = 'minioadmin'
 
     class Config:
         env_file = ".env"
